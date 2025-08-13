dcfe0505968533560403ca26ef6fc9d0a365258c
cainasoaresb <cainaasoares@gmail.com>
2025-08-13 13:45:30 -0300

    Adjust to home images

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as amplitude from '@amplitude/analytics-browser';
 
+import GryffindorAltEmblem from '/public/assets/img/Gryffindor.webp';
+import RavenclawAltEmblem from '/public/assets/img/Ravenclaw.webp';
+import HufflepuffAltEmblem from '/public/assets/img/Gryffindor.webp';
+import SlytherinAltEmblem from '/public/assets/img/Gryffindor.webp';
+
 const HouseCard = ({ house }) => {
   const { name, id } = house;
 
@@ -11,14 +16,24 @@ const HouseCard = ({ house }) => {
     });
   };
 
+
+  const houseEmblems = {
+    'Gryffindor': GryffindorAltEmblem,
+    'Ravenclaw': RavenclawAltEmblem,
+    'Hufflepuff': HufflepuffAltEmblem,
+    'Slytherin': SlytherinAltEmblem,
+  };
+
+  const emblemAltSrc = house ? houseEmblems[name] : null;
+
   return (
     <div className="md:flex md:shrink-0 p-6 rounded-lg flex flex-col items-center text-center m-4">
-      <img className='w-full object-cover md:h-full md:w-48' src={`/assets/img/${name}.webp`} alt={`${name}`} />      
+      <img className='w-full object-cover md:h-full md:w-48' src={emblemAltSrc} alt={`${name}`} />
       <h2 className="font-bold mb-4 text-white text-2xl leading-20">{name}</h2>
       <Link to={`/houses/${id}`}>
         <button
           className="rounded bg-violet-500 px-5 py-2 text-xl leading-7 font-bold text-white hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700"
-          onClick={handleSeeMoreClick} 
+          onClick={handleSeeMoreClick}
         >
           See more
         </button>
